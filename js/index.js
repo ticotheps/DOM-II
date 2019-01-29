770c533bf2e82f5cf7f192f7d4317f6f9f36aa6c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-29 16:03:16 -0500

    Added event listener #3 to index.js

--- js/index.js
+++ js/index.js
@@ -37,3 +37,65 @@ funBusH1.addEventListener('dblclick', e => {
   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
 });
 
+
+const funBusImage = document.querySelector('.fun-bus-image');
+
+// Event Listener #3 - 'mouseover'
+funBusImage.addEventListener('mouseover', e => {
+  console.log(`User has moved the pointing device on to the image of the toy bus in the sand, triggering this event: ${e}`);
+});
+
+
+// const funBusH1 = document.querySelector('.logo-heading');
+
+// // Event Listener #4 - 'wheel'
+// funBusH1.addEventListener('wheel', e => {
+//   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+// });
+
+
+// const funBusH1 = document.querySelector('.logo-heading');
+
+// // Event Listener #5 - 'drag'
+// funBusH1.addEventListener('drag', e => {
+//   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+// });
+
+
+// const funBusH1 = document.querySelector('.logo-heading');
+
+// // Event Listener #6 - 'load'
+// funBusH1.addEventListener('load', e => {
+//   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+// });
+
+
+// const funBusH1 = document.querySelector('.logo-heading');
+
+// // Event Listener #7 - 'focus'
+// funBusH1.addEventListener('focus', e => {
+//   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+// });
+
+
+// const funBusH1 = document.querySelector('.logo-heading');
+
+// // Event Listener #8 - 'resize'
+// funBusH1.addEventListener('resize', e => {
+//   console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+// });
+
+
+// // Event Listener #9 - 'scroll'
+// funBusH1.addEventListener('scroll', e => {
+//     console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+//   });
+
+
+// // Event Listener #10 - 'select'
+// funBusH1.addEventListener('select', e => {
+//     console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+//   });
+
+
+
