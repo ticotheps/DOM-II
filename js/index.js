8e27c0a8178ef15e3fba7dce3ce03243188fb36d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-01-29 15:56:19 -0500

    Added event listeners #1 + #2 to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-// Your code goes here
+
+// Using your index.js file, create 10 unique event listeners. using your
+// creativity to make the Fun Bus site more interactive. Here are some
+// unique events you could try to use:
+// -mouseover
+// -keydown
+// -wheel
+// -drag / drop
+// -load
+// -focus
+// -resize
+// -scroll
+// -select
+// -dblclick
+
+// Then, Using the 10 unique events, find ways to update the DOM in
+// creative ways. For example you could change colors, animate objects,
+// remove objects, etc.
+//   -Nest two similar events somewhere in the site and prevent the
+//   event propagation properly.
+//   -Stop the navigation from items from refreshing the page by
+//   using preventDefault()
+
+
+const signMeUpBtn1 = document.querySelector('.btn-1');
+
+// Event Listener #1 - 'click'
+signMeUpBtn1.addEventListener('click', e => {
+  console.log(`User has clicked on the "Sign Me Up!" button under the "Fun In The Sun" <div>, triggering this event: ${e}`);
+});
+
+
+const funBusH1 = document.querySelector('.logo-heading');
+
+// Event Listener #2 - 'dblclick'
+funBusH1.addEventListener('dblclick', e => {
+  console.log(`User has double-clicked on the "Fun Bus" text in the <header>, triggering this event: ${e}`);
+});
+
